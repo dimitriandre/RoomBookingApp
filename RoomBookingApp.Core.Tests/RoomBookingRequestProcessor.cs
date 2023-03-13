33bf95a0d5a246e410b36a1b54a014bf61a48edc
Dimitri Andre <dimitri@atmosclear.com>
2023-03-13 10:42:07 +0100

    Add first chapter code

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingAppCore
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        internal RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            return new RoomBookingResult
+            {
+                FullName = bookingRequest.FullName,
+                Email = bookingRequest.Email,
+                Date = bookingRequest.Date
+            };
+        }
+    }
+}
