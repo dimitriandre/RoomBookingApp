f8eefe6cf62f91567c1226a52f8f5d5a4e386b7f
dimitriandre <125253224+dimitriandre@users.noreply.github.com>
2023-07-19 16:48:39 +0200

    make the integration test for the application

--- RoomBookingApp.Persistance/Repositories/RoomBookingService.cs
+++ RoomBookingApp.Persistance/Repositories/RoomBookingService.cs
@@ -1,4 +1,5 @@
 ﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Domain.BaseModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,20 @@ using System.Threading.Tasks;
 namespace RoomBookingApp.Persistance.Repositories
 {
     public class RoomBookingService : IRoomBookingService
-    { 
+    {
+        private readonly RoomBookingAppDbContext _context;
+        public RoomBookingService(RoomBookingAppDbContext context)
+        { 
+            this._context = context;
+        }
+        public IEnumerable<Room> GetAvailableRooms(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(RoomBooking roomBooking)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
