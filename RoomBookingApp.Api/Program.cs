07f3c30af04277c3334d997a9ff74f1e048a44e1
dimitriandre <125253224+dimitriandre@users.noreply.github.com>
2023-07-25 08:26:48 +0200

    adding a database connexion

---
+++ RoomBookingApp.Api/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RoomBookingApp.Persistance;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Create connection to SQLite
+var connection = new SqliteConnection("DataSource=:memory:");
+connection.Open();
+
+builder.Services.AddDbContext<RoomBookingAppDbContext>(options =>
+    options.UseSqlite(connection));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
